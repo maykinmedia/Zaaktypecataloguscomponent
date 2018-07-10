cdb89bcb83594c07d69ff66799c1536d0a673ad2
Sonny Bakker <sonny@maykinmedia.nl>
2018-07-10 10:56:37 +0200

    Remove fields in test that are set by factory already

--- src/ztc/datamodel/tests/test_eigenschap_model.py
+++ src/ztc/datamodel/tests/test_eigenschap_model.py
@@ -6,64 +6,44 @@ from django.utils import timezone
 from ..models import Eigenschap
 
 from .factories import (
-    EigenschapFactory, EigenschapReferentieFactory, EigenschapSpecificatieFactory,
-    ZaakTypeFactory
+    EigenschapFactory, EigenschapReferentieFactory, EigenschapSpecificatieFactory
 )
 
 
 class EigenschapModelTests(TestCase):
     def test_model_raises_error_when_both_specificatie_and_referentie_are_set(self):
-        zaaktype = ZaakTypeFactory.create()
         specificatie = EigenschapSpecificatieFactory.create()
         referentie = EigenschapReferentieFactory.create()
 
         eigenschap = EigenschapFactory.create(
-            definitie="definitie",
-            eigenschapnaam="aard product",
-            toelichting="nieuw / verandering / ambtshalve wijziging / ontheffing / intrekking",
             specificatie_van_eigenschap=specificatie,
             referentie_naar_eigenschap=referentie,
-            is_van=zaaktype
         )
 
         self.assertRaises(ValidationError, eigenschap.clean)
 
     def test_model_raises_error_when_both_fields_are_not_set(self):
-        zaaktype = ZaakTypeFactory.create()
-
         eigenschap = EigenschapFactory.create(
-            definitie="definitie",
-            eigenschapnaam="aard product",
-            toelichting="nieuw / verandering / ambtshalve wijziging / ontheffing / intrekking",
-            is_van=zaaktype
+            specificatie_van_eigenschap=None,
+            referentie_naar_eigenschap=None,
         )
 
         self.assertRaises(ValidationError, eigenschap.clean)
 
     def test_model_does_not_raise_an_error_when_only_specificatie_is_set(self):
-        zaaktype = ZaakTypeFactory.create()
         specificatie = EigenschapSpecificatieFactory.create()
 
         eigenschap = EigenschapFactory.create(
-            definitie="definitie",
-            eigenschapnaam="aard product",
-            toelichting="nieuw / verandering / ambtshalve wijziging / ontheffing / intrekking",
             specificatie_van_eigenschap=specificatie,
-            is_van=zaaktype
         )
 
         self.assertIsNone(eigenschap.clean())
 
     def test_model_does_not_raise_an_error_when_only_referentie_is_set(self):
-        zaaktype = ZaakTypeFactory.create()
         referentie = EigenschapReferentieFactory.create()
 
         eigenschap = EigenschapFactory.create(
-            definitie="definitie",
-            eigenschapnaam="aard product",
-            toelichting="nieuw / verandering / ambtshalve wijziging / ontheffing / intrekking",
             referentie_naar_eigenschap=referentie,
-            is_van=zaaktype
         )
 
         self.assertIsNone(eigenschap.clean())
